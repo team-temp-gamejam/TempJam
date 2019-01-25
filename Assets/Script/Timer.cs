13906468b35e74a7509253673b41dd0e142a0b8a
Amornpong Trakarnkulphun <gearng2468@gmail.com>
2019-01-26 03:02:33 +0700

    Timer structure

---
+++ Assets/Script/Timer.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Threading;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField]
+    public int timeLeft; 
+    public Text timerText;
+
+    void Start()
+    {
+        StartCoroutine("TickTime");
+        //Time.timeScale = 1; 
+    }
+
+    void Update()
+    {
+        timerText.text = ("" + timeLeft);
+        
+        //lose the game
+        if(timeLeft == 0)
+        {
+
+        }
+
+    }
+
+
+    IEnumerator TickTime()
+    {
+        while (timeLeft>0)
+        {
+            yield return new WaitForSeconds(1);
+            timeLeft--;
+        }
+
+    }
+}
