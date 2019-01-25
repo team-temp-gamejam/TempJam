ef5044e90f58d50caa6a7e6b41284fa002ddc86a
Dnicx <theerapat.nick@gmail.com>
2019-01-26 03:33:34 +0700

    door and map in process

---
+++ Assets/Script/MapManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapManager : MonoBehaviour
+{
+
+    public int width, height;
+
+    public GameObject[] roomPrefab;
+
+    private List<List<GameObject>> room;
+
+    public GameObject GetRoom(int row, int column) {
+        return room[row][column];
+    }
+}
---
+++ Assets/Script/RoomScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomScript : MonoBehaviour
+{
+    [SerializeField]
+    private int row, column;
+
+    public GameObject LeftDoor, RightDoor, UpDoor, DownDoor;
+
+    public void setTilePosition(int row, int column) {
+        this.column = column;
+        this.row = row;
+    }
+
+    public Vector2 getTilePosition() {
+        return new Vector2(row, column);
+    }
+
+    
+}
