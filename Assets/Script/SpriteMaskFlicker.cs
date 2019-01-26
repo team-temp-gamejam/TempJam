35b770b816f7e2820a6efceae8f4f89ba473b985
Dnicx <theerapat.nick@gmail.com>
2019-01-26 19:35:37 +0700

    darkness mask

---
+++ Assets/Script/SpriteMaskFlicker.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteMaskFlicker : MonoBehaviour
+{
+    // Start is called before the first frame update
+    SpriteMask mask;
+    public float timeToChange;
+    private float counter;
+    private int current = 1;
+
+    public Sprite mk1, mk2, mk3;
+
+    void Start()
+    {
+        mask = GetComponent<SpriteMask>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        counter += Time.deltaTime;
+        if (counter > timeToChange) {
+            counter = 0;
+            current = (current+1)%2;
+            if (current == 0 ) mask.sprite = mk1;
+            if (current == 1 ) mask.sprite = mk2;
+            if (current == 2 ) mask.sprite = mk3;
+        }
+    }
+}
