2f8ed783e576a4554b31ec6dd74d1525ef432ff9
Dnicx <theerapat.nick@gmail.com>
2019-01-28 18:00:18 +0700

    correcting camera-item layering

--- Assets/Script/CupboardScript.cs
+++ Assets/Script/CupboardScript.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class CupboardScript : InteractItem
 {
+    [SerializeField]
     private PlayerControl interactingPlayer;
 
     // Start is called before the first frame update
